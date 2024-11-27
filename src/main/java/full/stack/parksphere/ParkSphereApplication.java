cfdf2eb4e1f84e81de350b0a920afed8da97a9e1
Alamin-T <tmyhammed@gmail.com>
2024-11-27 17:13:32 +0100

    setting up DB

--- src/main/java/full/stack/parksphere/ParkSphereApplication.java
+++ src/main/java/full/stack/parksphere/ParkSphereApplication.java
@@ -2,8 +2,10 @@ package full.stack.parksphere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "full.stack.parksphere")
 public class ParkSphereApplication {
 
     public static void main(String[] args) {
